841e95253068a6464691a7b2c542a6845aa241eb
Daniel Gutierrez <jairogutd@gmail.com>
2024-06-05 22:39:15 -0500

    gte reference for players in player manager

--- Deck Fight!!!/Assets/Scripts/PlayerSpawner.cs
+++ Deck Fight!!!/Assets/Scripts/PlayerSpawner.cs
@@ -6,13 +6,17 @@ using UnityEngine.InputSystem;
 public class PlayerSpawner : MonoBehaviour
 {
 
+    public GameObject player;
     public GameObject playerOne;
+    public GameObject playerTwo;
     // Start is called before the first frame update
     void Start()
     {
-        var player1 = PlayerInput.Instantiate(prefab: playerOne, playerIndex: 0, controlScheme: "WASD", pairWithDevice: Keyboard.current);
-        var player2 = PlayerInput.Instantiate(prefab: playerOne, playerIndex: 0, controlScheme: "Numpad", pairWithDevice: Keyboard.current);
+        var player1 = PlayerInput.Instantiate(prefab: player, playerIndex: 0, controlScheme: "WASD", pairWithDevice: Keyboard.current);
+        var player2 = PlayerInput.Instantiate(prefab: player, playerIndex: 0, controlScheme: "Numpad", pairWithDevice: Keyboard.current);
         player1.transform.position = new Vector2(-5, -2.5f);
         player2.transform.position = new Vector2(5, -2.5f);
+        playerOne = GameObject.FindGameObjectWithTag("Player 1");
+        playerTwo = GameObject.FindGameObjectWithTag("Player 2");
     }
 }
